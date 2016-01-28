f033d897220c52e31d973243f8221e43eeef32b6
toomanyadverbs <lark.grimes@gmail.com>
2016-01-28 12:49:20 -0500

    Encountered problems with the downloaded jQuery file; replaced with the jQuery kindly hosted by Google

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 var url = "https://data.cityofboston.gov/resource/4swk-wcg8.json";
 
-$("button").click(function()
-{
-$.get( url, function( data ) {
-  $( ".result" ).html( data );
-  console.log( "Load was performed." );
-});
+
+$("button").click(function(){
+	$.get( url, function( data ) {
+  		$( ".result" ).html( data );
+  		console.log( "Load was performed." );
+	});
 });
